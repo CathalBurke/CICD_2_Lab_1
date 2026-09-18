ba898ee25ad8e5993b4e58a47eea0b178be9302f
Cathal Burke <150245282+CathalBurke@users.noreply.github.com>
2026-09-18 11:00:24 +0000

    Added exception handeling to deal with duplicates for matching objects

--- app/main.py
+++ app/main.py
@@ -13,5 +13,11 @@ def hello ():
     return {"message": "hello world"}
 @app.post("/api/users", status_code=status.HTTP_201_CREATED)
 def add_user(new_user:UserCreate):
+    for existing_user in users:
+        if existing_user.userid == new_user.userid:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="A user with this User ID already exists"
+            )
     users.append(new_user)
     return new_user
